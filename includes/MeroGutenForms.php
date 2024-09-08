074d10e68b76597a565588c924825beff30878a0
milan88888 <chaudharymilan996@gmail.com>
2024-09-08 22:12:16 +0545

    Add - Basic plugin setup file composer singleton class

---
+++ includes/MeroGutenForms.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * MeroGutenForms main class.
+ *
+ * This file is responsible for managing all blocks and block categories
+ * used within the MeroGutenForms plugin. It includes functionality for
+ * registering, rendering, and categorizing blocks to be used in the
+ * Gutenberg editor.
+ *
+ * @since 1.0.0
+ * @package MeroGutenForms
+ */
+
+namespace MeroGutenForms;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+use MeroGutenForms\Traits\Singleton;
+
+/**
+ * MeroGutenForms setup.
+ *
+ * Include and initialize necessary files and classes for the plugin.
+ *
+ * @since   1.0.0
+ */
+final class MeroGutenForms {
+
+	use Singleton;
+
+	/**
+	 * Plugin Constructor.
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 */
+	protected function __construct() {
+		error_log( 'in' );
+	}
+}
--- mero-guten-forms.php
+++ mero-guten-forms.php
@@ -17,15 +17,59 @@
  */
 
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
-! defined('MGF_VERSION') && define('MGF_VERSION', '1.0.0');
-! defined('MGF_PLUGIN_FILE') && define('MGF_PLUGIN_FILE', __FILE__);
-! defined('MGF_PLUGIN_DIR') && define('MGF_PLUGIN_DIR', dirname(MGF_PLUGIN_FILE));
-! defined('MGF_PLUGIN_DIR_URL') && define('MGF_PLUGIN_DIR_URL', plugin_dir_url(MGF_PLUGIN_FILE));
-! defined('MGF_DIST_DIR') && define('MGF_DIST_DIR', MGF_PLUGIN_DIR . '/dist');
-! defined('MGF_ASSETS_DIR_URL') && define('MGF_ASSETS_DIR_URL', MGF_PLUGIN_DIR_URL . 'assets');
-! defined('MGF_DIST_DIR_URL') && define('MGF_DIST_DIR_URL', MGF_PLUGIN_DIR_URL . 'dist');
-! defined('MGF_LANGUAGES') && define('MGF_LANGUAGES', MGF_PLUGIN_DIR . '/languages');
-! defined('MGF_UPLOAD_DIR') && define('MGF_UPLOAD_DIR', wp_upload_dir()['basedir'] . '/mero-guten-forms');
-! defined('MGF_UPLOAD_DIR_URL') && define('MGF_UPLOAD_DIR_URL', wp_upload_dir()['baseurl'] . '/mero-guten-forms');
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+} else {
+	/**
+	 * Outputs an admin notice if composer install has not been ran.
+	 */
+	add_action(
+		'admin_notices',
+		function () {
+			?>
+			<div class="notice notice-error">
+				<p>
+					<?php
+					printf(
+						/* translators: 1: composer command. 2: plugin directory */
+						esc_html__( 'Your installation of the  MeroGutenForms plugin is incomplete. Please run %1$s within the %2$s directory.', 'mero-guten-forms' ),
+						'<code>composer install</code>',
+						'<code>' . esc_html( str_replace( ABSPATH, '', __DIR__ ) ) . '</code>'
+					);
+					?>
+				</p>
+			</div>
+			<?php
+		}
+	);
+	return;
+}
+
+use MeroGutenForms\MeroGutenForms;
+
+! defined( 'MGF_VERSION' ) && define( 'MGF_VERSION', '1.0.0' );
+! defined( 'MGF_PLUGIN_FILE' ) && define( 'MGF_PLUGIN_FILE', __FILE__ );
+! defined( 'MGF_PLUGIN_DIR' ) && define( 'MGF_PLUGIN_DIR', dirname( MGF_PLUGIN_FILE ) );
+! defined( 'MGF_PLUGIN_DIR_URL' ) && define( 'MGF_PLUGIN_DIR_URL', plugin_dir_url( MGF_PLUGIN_FILE ) );
+! defined( 'MGF_DIST_DIR' ) && define( 'MGF_DIST_DIR', MGF_PLUGIN_DIR . '/dist' );
+! defined( 'MGF_ASSETS_DIR_URL' ) && define( 'MGF_ASSETS_DIR_URL', MGF_PLUGIN_DIR_URL . 'assets' );
+! defined( 'MGF_DIST_DIR_URL' ) && define( 'MGF_DIST_DIR_URL', MGF_PLUGIN_DIR_URL . 'dist' );
+! defined( 'MGF_LANGUAGES' ) && define( 'MGF_LANGUAGES', MGF_PLUGIN_DIR . '/languages' );
+! defined( 'MGF_UPLOAD_DIR' ) && define( 'MGF_UPLOAD_DIR', wp_upload_dir()['basedir'] . '/mero-guten-forms' );
+! defined( 'MGF_UPLOAD_DIR_URL' ) && define( 'MGF_UPLOAD_DIR_URL', wp_upload_dir()['baseurl'] . '/mero-guten-forms' );
+
+
+if ( ! function_exists( 'mero_init' ) ) {
+	/**
+	 * Returns the main instance of MeroGutenForms to prevent the need to use globals.
+	 *
+	 * @return MeroGutenForms|null
+	 */
+	function mero_init() {
+		return MeroGutenForms::instance();
+	}
+
+	mero_init();
+}
